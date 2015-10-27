ee7888eda2ece5f88f53d6946b212c7a1f81a07a
Shanan Sussman <gfxbox@gmail.com>
2015-10-27 15:21:30 -0700

    major refactor to rebuilding timelines, keyframe drag still WIP

--- client/timeline.js
+++ client/timeline.js
@@ -66,8 +66,12 @@ window.onload = function() {
     return ((x - prevX) / scale.x.pixels) * scale.x.ratio;
   };
 
-  var adjustValue = function(y, prevY, prevPixelY){
+  var adjustValue = function(y, prevY, prevPixelY) {
     return (((y - prevY) / scale.y.pixels) * scale.y.ratio) + prevPixelY;
+  };
+
+  var adjustPrevPixelY = function(y, prevY, nextPixelY) {
+    return -((((y - prevY) / scale.y.pixels) * scale.y.ratio) - nextPixelY);
   };
 
   var recalcEase = function(tween) {
@@ -77,24 +81,37 @@ window.onload = function() {
       var segmentSelected = globals.recalc.segmentSelected;
       var prevTween = segmentPrev ? timeline.getChildren()[segmentPrev.index] : null;
       var nextTween = segmentNext ? timeline.getChildren()[segmentSelected.index] : null;
-      var keyframe = globals.recalc.keyframe
+      var keyframe = globals.recalc.keyframe;
+
+
       if (keyframe) {
         if (prevTween) {
           // console.log(prevTween);
           var tweenData = globals.xTSpline.tweenData[segmentPrev.index];
           var recalcDuration = adjustTime(keyframe.point.x, tweenData.prevCoordX);
           var recalcValue = adjustValue(keyframe.point.y, tweenData.prevCoordY, tweenData.prevPixelY);
-          // console.log('recalc duration & value', recalcDuration, recalcValue, prevTween.vars.css);
+          // console.log('prevTweeeeeeeen---> recalcValue',recalcValue, 'keyframe Y', keyframe.point.y, 'tween prevCoordY', tweenData.prevCoordY, '    prevPixel', tweenData.prevPixelY);
 
+          // prevTween.invalidate();
+          // prevTween.vars.css.left = recalcValue + 'px';
+          // prevTween.duration(recalcDuration);
+          tweenData.adjustedTime = recalcDuration;
+          tweenData.adjustedValue = recalcValue;
 
-          prevTween.invalidate();
-          prevTween.vars.css.left = recalcValue + 'px';
         }
         if (nextTween) {
           var tweenData = globals.xTSpline.tweenData[segmentSelected.index];
-          console.log('tween data :', tweenData)
+          var recalcDuration = adjustTime(segmentNext.point.x, keyframe.point.x);
+          var recalcValue = adjustPrevPixelY(segmentNext.point.y, keyframe.point.y, tweenData.adjustedValue);
+          
           // nextTween.invalidate();
+          // nextTween.vars.startAt.left = recalcValue + 'px';
+          // nextTween.duration(recalcDuration);
+          tweenData.adjustedTime = recalcDuration;
+          tweenData.prevPixelY = recalcValue;
+
         }
+        rebuildTimeline();
       }
       else {
         if (prevTween){
@@ -115,7 +132,6 @@ window.onload = function() {
   var createKeyFrame = function(x, y) {
     var insertionIndex = keyFrameInsertionCheck(x, y);
 
-    console.log('insertionIndex', insertionIndex)
     globals.drawKeyFrame(x, y, insertionIndex);
     var totalKeyFrames = globals.xTSpline.segments.length;
 
@@ -124,47 +140,62 @@ window.onload = function() {
 
       if (insertionIndex === null){
         console.log('null insertion');
+        var length = globals.xTSpline.tweenData.length;
+        var previousTweenData = length ? globals.xTSpline.tweenData[length - 1] : globals.xTSpline.firstKey;
+        // console.log(previousTweenData);
 
-        var adjustedTime = adjustTime(x, prevCoordX);
-        var adjustedValue = adjustValue(y, prevCoordY, prevPixelY);
 
-        timeline.fromTo(box1, adjustedTime, {left: prevPixelY + "px"}, {left: adjustedValue + "px", onUpdate: recalcEase, onUpdateParams:["{self}"], ease: updateEase(getEaseArray(globals.xTSpline.segments[totalKeyFrames - 2], globals.xTSpline.segments[totalKeyFrames - 1]))});
-        globals.xTSpline.tweenData.push({element: box1, adjustedTime: adjustedTime, prevPixelY: prevPixelY, adjustedValue: adjustedValue, prevCoordX: prevCoordX, prevCoordY: prevCoordY});
-        prevPixelY = adjustedValue;
-        
+        var adjustedTime = adjustTime(x, previousTweenData.currentCoordX);
+        var adjustedValue = adjustValue(y, previousTweenData.currentCoordY, previousTweenData.adjustedValue);
+
+        timeline.fromTo(box1, adjustedTime, {left: previousTweenData.adjustedValue + "px"}, {left: adjustedValue + "px", onUpdate: recalcEase, onUpdateParams:["{self}"], ease: updateEase(getEaseArray(globals.xTSpline.segments[totalKeyFrames - 2], globals.xTSpline.segments[totalKeyFrames - 1]))});
+        globals.xTSpline.tweenData.push({element: box1, adjustedTime: adjustedTime, prevPixelY: previousTweenData.adjustedValue, adjustedValue: adjustedValue, prevCoordX: previousTweenData.currentCoordX, prevCoordY: previousTweenData.currentCoordY, currentCoordX: x, currentCoordY: y});
+        // prevPixelY = adjustedValue;
+        console.log(globals.xTSpline.tweenData);
       }
-
       else {
-
-        timeline.clear();
-
-        for (var i = 0; i < globals.xTSpline.tweenData.length; i ++) {
-          var currentTween = globals.xTSpline.tweenData[i];
-
-          if (i === insertionIndex) {
-            var insertAdjustedTime = adjustTime(x, currentTween.prevCoordX);
-            var insertAdjustedValue = adjustValue(y, currentTween.prevCoordY, currentTween.prevPixelY);
-            var remainderTime = currentTween.adjustedTime - insertAdjustedTime;
-
-            timeline.fromTo(currentTween.element, insertAdjustedTime, {left: currentTween.prevPixelY + "px"}, {left: insertAdjustedValue + "px", onUpdate: recalcEase, onUpdateParams:["{self}"], ease: updateEase(getEaseArray(globals.xTSpline.segments[insertionIndex], globals.xTSpline.segments[insertionIndex + 1]))});
-
-            timeline.fromTo(currentTween.element, remainderTime, {left: insertAdjustedValue + "px"}, {left: currentTween.adjustedValue + "px", onUpdate: recalcEase, onUpdateParams:["{self}"], ease: updateEase(getEaseArray(globals.xTSpline.segments[insertionIndex + 1], globals.xTSpline.segments[insertionIndex + 2]))});
-            globals.xTSpline.tweenData.splice(i + 1, 0, {element: box1, adjustedTime: remainderTime, prevPixelY: insertAdjustedValue, adjustedValue: currentTween.adjustedValue, prevCoordX: x, prevCoordY: y});
-            currentTween.adjustedTime = insertAdjustedTime;
-            currentTween.adjustedValue = insertAdjustedValue;
-            
-            i++;
-          }
-          else {
-            timeline.fromTo(currentTween.element, currentTween.adjustedTime, {left: currentTween.prevPixelY + "px"}, {left: currentTween.adjustedValue + "px", onUpdate: recalcEase, onUpdateParams:["{self}"], ease: updateEase(getEaseArray(globals.xTSpline.segments[i], globals.xTSpline.segments[i + 1]))});
-          }
-        }
+        rebuildTimeline({x:x, y:y, insertionIndex: insertionIndex});
       }
     }
 
-    if (insertionIndex === null) {
-      prevCoordX = x;
-      prevCoordY = y;
+    if (totalKeyFrames === 1) {
+      globals.xTSpline.firstKey = {currentCoordX: x, currentCoordY: y, adjustedValue: 100};
+    }
+  };
+
+  var rebuildTimeline = function(insertionObject) {
+    insertionObject = insertionObject || null;
+    timeline.clear();
+
+    if (insertionObject) {
+      for (var i = 0; i < globals.xTSpline.tweenData.length; i++) {
+        var currentTween = globals.xTSpline.tweenData[i];
+
+        if (i === insertionObject.insertionIndex) {
+
+          var insertAdjustedTime = adjustTime(insertionObject.x, currentTween.prevCoordX);
+          var insertAdjustedValue = adjustValue(insertionObject.y, currentTween.prevCoordY, currentTween.prevPixelY);
+          var remainderTime = currentTween.adjustedTime - insertAdjustedTime;
+
+          timeline.fromTo(currentTween.element, insertAdjustedTime, {left: currentTween.prevPixelY + "px"}, {left: insertAdjustedValue + "px", onUpdate: recalcEase, onUpdateParams:["{self}"], ease: updateEase(getEaseArray(globals.xTSpline.segments[insertionObject.insertionIndex], globals.xTSpline.segments[insertionObject.insertionIndex + 1]))});
+
+          timeline.fromTo(currentTween.element, remainderTime, {left: insertAdjustedValue + "px"}, {left: currentTween.adjustedValue + "px", onUpdate: recalcEase, onUpdateParams:["{self}"], ease: updateEase(getEaseArray(globals.xTSpline.segments[insertionObject.insertionIndex + 1], globals.xTSpline.segments[insertionObject.insertionIndex + 2]))});
+          globals.xTSpline.tweenData.splice(i + 1, 0, {element: box1, adjustedTime: remainderTime, prevPixelY: insertAdjustedValue, adjustedValue: currentTween.adjustedValue, prevCoordX: insertionObject.x, prevCoordY: insertionObject.y});
+          currentTween.adjustedTime = insertAdjustedTime;
+          currentTween.adjustedValue = insertAdjustedValue;
+          
+          i++;
+        }
+        else {
+          timeline.fromTo(currentTween.element, currentTween.adjustedTime, {left: currentTween.prevPixelY + "px"}, {left: currentTween.adjustedValue + "px", onUpdate: recalcEase, onUpdateParams:["{self}"], ease: updateEase(getEaseArray(globals.xTSpline.segments[i], globals.xTSpline.segments[i + 1]))});
+        }
+      }
+    }
+    else {
+      for (var i = 0; i < globals.xTSpline.tweenData.length; i++) {
+        var currentTween = globals.xTSpline.tweenData[i];
+        timeline.fromTo(currentTween.element, currentTween.adjustedTime, {left: currentTween.prevPixelY + "px"}, {left: currentTween.adjustedValue + "px", onUpdate: recalcEase, onUpdateParams:["{self}"], ease: updateEase(getEaseArray(globals.xTSpline.segments[i], globals.xTSpline.segments[i + 1]))});
+      }
     }
   };
 
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.all('*', function(req, res, next) {
+       res.header("Access-Control-Allow-Origin", "*");
+       res.header("Access-Control-Allow-Headers", "X-Requested-With");
+       res.header('Access-Control-Allow-Headers', 'Content-Type');
+       next();
+});
 app.use(express.static(__dirname + '/client'));
+
 
 var port = process.env.PORT || 8000;
 
